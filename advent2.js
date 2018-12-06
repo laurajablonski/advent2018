84936f949276d07de653c45248e2c6c91a58d380
Laura Jablonski <lhjablonski@gmail.com>
2018-12-05 21:40:07 -0500

    solution for problem 2

--- advent2.js
+++ advent2.js
@@ -1,3 +1,74 @@
 document.writeln('Advent of Code Problem 2');
 
 var boxIDs = ["uqcipadzntnheslgvjjozmkfyr","uqcipadzwtnhexlzvxjobmkfkr","cqcipadpwtnheslgyxjobmkfyr","ubnipadzwtnheslgvxjobmkfyw","uqcisadzwtnheslgvxjfbmkfor","uqcisaezwtnheslgvxkobmkfyr","uqcguadzwtnheslgvxjobmkfir","uqcipadzmtnhesldvxdobmkfyr","uqcipadzwtzheslgdxjtbmkfyr","uquipadzwtcheslgvxjobmkfbr","uqctpadzwtnhesjbvxjobmkfyr","ueciparzwtnheslgvxjobmkfyx","uqcipadzwtnhessgvxjkbmkfkr","uqcipamzwtnheslgvxioamkfyr","uciizadzwtnheslgvxjobmkfyr","uqcieadzwtnhesfgvxeobmkfyr","fqcipadzwtnreslgvkjobmkfyr","uqcipadzrtnherlgvxjobmklyr","uqcypadzwtnheslgvxjobmkxfr","uqcipadzwtnhemlgvxjobmvfur","uwciuadzwwnheslgvxjobmkfyr","uqcipadzwtnhcscgvxjobmkuyr","upripadzwtnheslovxjobmkfyr","uqcipadzltnheslgvxjobmkftc","uqcipadzwtnheslgvgjobmifsr","uqoipadzwtnheslgvxjosmkfkr","uqcipadzwtbhesrqvxjobmkfyr","uqcipadzwtnheslpvxjobmhfyx","uhcinadzwtnheslgvxjybmkfyr","uqcipadzwtnhhslgvxjabmkbyr","uecipadzwtnheslgvxjobqyfyr","uqcipadfwtnheslwvxjobgkfyr","uqcipadzvtnheshgvxzobmkfyr","fqcipadzwtcheslgvxjobmkfyt","uecipadzwtnheslgpxjbbmkfyr","uqclpadzwtnheslgvnjobukfyr","qqciprdzetnheslgvxjobmkfyr","uqcipahpwtnheslgvxjtbmkfyr","uqcidadzwtnhesljvxyobmkfyr","uqciradswtnqeslgvxjobmkfyr","uqcipadzwtrhmslgvxjobmkfyf","urcipadzjtnheslgvxfobmkfyr","uqcipadzwznheslgvxjobmkfcv","uqcipadowtnheslgyxjobmkfym","uqcigadzwtnheslgvxjoomkmyr","uqjipafzwtnheslgvejobmkfyr","uqcioadzwtnhhslgvxzobmkfyr","uqcgpadkwtnheslgvxjobhkfyr","ufciiadewtnheslgvxjobmkfyr","uqoipadzwtnheslgvxjllmkfyr","uqcipadzutnheslgwxxobmkfyr","uqcipadzwtlheslgaxjobmkfwr","uqcbpadzutnheslgvxjbbmkfyr","uucipadzwvnhesngvxjobmkfyr","uqcifadzwtnceslgvxjoumkfyr","ujcipadzwteheslgvxjobmkfyj","uqcipadzwtnheslqvxjobmkuyp","uqcipadzwtnheslgvxjoxmkxyw","uqcipaduwtnheslgvujmbmkfyr","uicipadnwtnheslgvxjobmbfyr","uqcipadzwteheslgvxjobbmfyr","uqcipadzwgnneslgvxjobmklyr","uqcipadzxtnhwslgvbjobmkfyr","uqcipaxwwtnheslxvxjobmkfyr","uocipadzwtnheslgvxjobqdfyr","uqciaauzwtnheslgtxjobmkfyr","uncipagzwtnkeslgvxjobmkfyr","uqcipadzwtnhehlgvxjohdkfyr","uqcipadzwtnheslgvxjobmspyz","uccipadzwtnhvsltvxjobmkfyr","uacipagzwtnheslgvxjoqmkfyr","tqcipaduwtnheslgvxjobmmfyr","uqcipadzwtnheslgvxqebmifyr","uecipadthtnheslgvxjobmkfyr","uocipadzwtnhdslgvkjobmkfyr","uqcipadtwtnheslgvxhobmufyr","uqkipadzwtnleslgtxjobmkfyr","uqcipadzjunheslgvxjobmnfyr","ubcipadzwtvheslgvxjobmkfyf","uqcipadzwpfheslgvxjsbmkfyr","uocipadzwtndeslgvxjobmmfyr","uqcipadzwtnheslgtxjobhkfyq","uqcipadzwtrheslgvxjobmyfya","uqcipadzwtvheslgvxjolgkfyr","uqcipidzwtaheslgvxjobmkfxr","uyzixadzwtnheslgvxjobmkfyr","uqyihadzwtnhedlgvxjobmkfyr","uqcipadzwtnhesltvejobqkfyr","uqciptdzwtnheslgyxlobmkfyr","uqcipzdzwtnhzslgvxjosmkfyr","uqcipadzwtnbeslgexjobmkfvr","uqcipadzwtnheslcwxjobmkkyr","uqcapadzwcnheslgvxjolmkfyr","uqcjpadzwtnhejlgvxjxbmkfyr","uqcipadwwtxweslgvxjobmkfyr","uqmipadzwtnhezlgvxjobmkyyr","uqcipubzwtnpeslgvxjobmkfyr","uecvpadzwtnheslgvxjocmkfyr","uqcipadzwfnheslgvxjibmkdyr","uqcipadzwtnheslgvxvfbykfyr","uqcipadzwtnheslgvgjoimkfyt","dqcqpaqzwtnheslgvxjobmkfyr","uqcipbdzwtnheslgvxjobmkghr","jqcipadzwtnheslgvxjgbmkzyr","uqcipadzwtnheslgvxqkqmkfyr","uqcipadzptnheslgvxjxbokfyr","uucijadzwtwheslgvxjobmkfyr","uccfpadzwtnheslgvxjobpkfyr","uqcipadzwtnheslgvxjobakeyq","uqcipadzwtnheolgvxqobjkfyr","imiipadzwtnheslgvxjobmkfyr","uqcehadzwtnheslgvxjobmkuyr","uqcipadzztnheslgvxjorokfyr","rqcixadzwtnheelgvxjobmkfyr","uqcipadzwtzheslgvxjodmkfyi","uqcipaezwtnwuslgvxjobmkfyr","uqcipadzwtnheslggxjobjkfyq","uqcipadzwkghesagvxjobmkfyr","uqcypqdzwtnheslgvxjobakfyr","iqcipadzwtnhezltvxjobmkfyr","uxcimadzwtnheslgvxjobmxfyr","uqcipaizwtvhwslgvxjobmkfyr","uqcipafzwtnheslgvxjpbmkfym","uqcipadzwinheslgvxlobmpfyr","uqcupadzwtnheslkvxmobmkfyr","uqcapadzwtnhesrgvxjobmkfsr","urcipafzwtnheslgvxjobmkfur","uqcipaczwtnheslgvbjobmknyr","uqcizadzztgheslgvxjobmkfyr","uqcipfdzwtnhesxgvxjobmkfyw","uqcipbdzwtnhyslgvxjobmcfyr","uqcipadzwanhezlgvxjobmkfwr","uvcipadzwtnheslgvxjbkmkfyr","uqcipajzwtnseslgvxjobmkfyq","uqcipvdzwtnheslgvmlobmkfyr","uqcipadzdgnheslgmxjobmkfyr","uqcipddzwtnhestgvpjobmkfyr","umcipadzwtdheslgvxjzbmkfyr","uqciuwdzwtnheslgvxjobmkflr","uqcipadzwtnheslgsxabbmkfyr","uceipadzwtnheslgvxjobgkfyr","mqcipadzwtnhesrgvxjobmjfyr","aqcipadvwtnheslgvxjobmkryr","uqsipadzwtnofslgvxjobmkfyr","uqcixadzwtfheslgvxjzbmkfyr","uqcipadnwfnheslgvxjohmkfyr","uqcivadzwtnheslfvxjobmkfyz","uqciprdzwtnheslgvxjobmkjir","uqcipadhbtnheslgvxjoxmkfyr","fqcipadzwtnhesfgvxjobmkfye","uqoipqdzwtnheqlgvxjobmkfyr","uqcipadzwtnhesltvxmobmkzyr","uqcipadzwtnhebqgvsjobmkfyr","uqcipadzwtnheslglxjobmfbyr","gqcipadzwtgheslgvxjobwkfyr","uqcipadzwtnheslgfxjzbmlfyr","ujcnpadzwtnheslrvxjobmkfyr","ujcivadzwtnheglgvxjobmkfyr","uqcitadzwgnheslgvxjofmkfyr","uqcipahzatnhmslgvxjobmkfyr","uqzipaizwtnheslgvujobmkfyr","uqcipadzltnheylgvnjobmkfyr","uqcidadzwtnhwsljvxyobmkfyr","uqcipadzwtihetlgvxjobhkfyr","oqcipabzwtnheslgvfjobmkfyr","uqcipadzwtnveslgvxjobzkfzr","uqcipadzwtjheslgqxjobmlfyr","uqcnpadzztnheslgvxjobmkoyr","uqciuadzwonheslgvxjobmkfyz","tqcipadzwtnheslgvxaobmqfyr","uqcipadtwtnhqslgvxjobmkeyr","uqcipadzwbnheslgvajobmsfyr","ubcopadzwtnhgslgvxjobmkfyr","uqcipydzwtwheslgvxjobakfyr","cqbijadzwtnheslgvxjobmkfyr","uscipadowtnheslgvxjobmkfcr","uqcipadzwtgheslnvxjobskfyr","uqcipzdzwtnzeslgkxjobmkfyr","uqcipawzwtnhrslgbxjobmkfyr","uqcipadzatchyslgvxjobmkfyr","uqcipadzotnpeslgvxjobmjfyr","uqcipagzwtnheslgvxjobmvfyt","uqcipadzwhnheslgvxyobmkfyo","uqcipadzwtnheslgmqjobmkfyc","uqcupadzwgnheslgvcjobmkfyr","uqcipabzwbnheslgvxjobmkwyr","uqciiadzwtnheslgvxjobmkfmz","uqkipauzwtnheslgvxjjbmkfyr","uqcipidzetnheslgvxjobmkfyi","uqcipadzwtnheslgqxjokmkfmr","uqcipadzqtnhesllvxjobmkfyk","uqccpadzwtnheslgmxsobmkfyr","uqcipadzwteheslgvljfbmkfyr","uqcipadxwinheslgaxjobmkfyr","uqcipadzwtnheslhvxyobmkfjr","aqcipadzwnnheslgvxjqbmkfyr","uvcipadzwtnheszgvxjobmkfyg","uqcipahzmtnheslgvxjobmkfir","ukcipadzbtnheslgvxjobmkfyb","uqcipadzwtnhemlgvqjobmkfpr","uqcipadzwtnheslgvmeobmkfpr","uqciphdrwtnheslgvxjobmkfyw","uqcipadzwtnheslevxqobzkfyr","uqcipadzwknzeslgvxnobmkfyr","wqcipadzwjnheslgvxjobbkfyr","uqcipadzwtdheslgvmjobmkjyr","uqvipadzwtnhextgvxjobmkfyr","uqhipadzwtnheslwvxjzbmkfyr","uqcipadzwtnherlgsxjobmksyr","uqcipadzwtnhesqgvxjotmvfyr","udcipadzwtnhekwgvxjobmkfyr","uqcjprdzwtnheslgvxjobmkfpr","uqcipadzatnheclgvqjobmkfyr","uqcbpadzctnheslqvxjobmkfyr","uqcipadzqtnhesluvxjobrkfyr","uqcipadzwtnhcslgvxjoomwfyr","uqcppadzwxnheslgwxjobmkfyr","uqcipadcwtnheslrvxjdbmkfyr","ukcipadzwtnhhslgvxjobmkgyr","uqckpadzwtnheslgvxjokmkiyr","uqcspadzwtjheslgvxjobmkfjr","uqcipadpwtnhsslgvxjobmkfyu","uqcepadzwtnheilgvbjobmkfyr","jqcipadiwtnheslgvxjobmkjyr","uqcipadzrtnseslgqxjobmkfyr","sqmipadzwtnhewlgvxjobmkfyr","uqcieadzhtnheslgvgjobmkfyr","uqcipadzwkwhewlgvxjobmkfyr","uqcipadzwtzheslgvxjpbqkfyr","uzcipadzjtnheslgvxjobmlfyr","uqcipadzwtnheslnvxjobmkfee","uqciyanzwtnheslgvxjoimkfyr","uqcipadqwtnheswghxjobmkfyr","uycipadzwtnheslovxjobmofyr","uqcipadzwtnheslgvxcozmxfyr","uqmipadzwtnxezlgvxjobmkfyr","uqcipadzftnheslgvxjotmkffr","aqcipaizwtnhesagvxjobmkfyr","uqcipcdzwtnheslgoajobmkfyr","uqcypadgwtnhesbgvxjobmkfyr","uqcipcdzwtnheslgvxjebmkfyb","uhcvpadzwtnheslgvxjobzkfyr","uqcipadzwtnpesagvxmobmkfyr","uqcipadzwtnidslgvxjobmkfor","uqcipadkwtnhesigvxjzbmkfyr","uqcypadlwtnheslsvxjobmkfyr","qqcipadzwtnheswgvxjobmkoyr","uqcipadzwtnheslgvxjhbmmcyr","uqcipadzwtnhesogvxjormkfmr","uqcipadzwtnhetcgvxgobmkfyr"];
+
+//Part 1
+document.writeln('Part 1');
+
+//Create an array containing the count of repeats for each boxID
+var i;
+var j;
+var counter;
+var idDuplicates = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var sumIDDuplicates = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+
+for (boxID in boxIDs) {
+   idDuplicates = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+   for (i = 0; i < boxIDs[boxID].length; i++) {
+     counter = 0;
+     for (j = 0; j < boxIDs[boxID].length; j++) {
+        if (boxIDs[boxID][i] == boxIDs[boxID][j]) {
+          counter++;
+        }
+     }
+     idDuplicates[counter] = 1;
+   }
+   console.log("ID: " + boxIDs[boxID]
+                + ", Duplicates: " + idDuplicates.toString());
+   for (i = 0; i < idDuplicates.length; i++){
+     sumIDDuplicates[i] += idDuplicates[i];
+   }
+}
+
+document.writeln('Count of 2 letter IDs: '+ sumIDDuplicates[2].toString());
+document.writeln('Count of 3 letter IDs: '+ sumIDDuplicates[3].toString());
+document.writeln('Checksum 2x3: ' +
+                (sumIDDuplicates[2]*sumIDDuplicates[3]).toString());
+
+//Part 2
+document.writeln('Part 2');
+
+var result = {
+   maxLength: 0,
+   id1: "",
+   id2: "",
+   commonLetters: "",
+};
+
+var i;
+var j;
+var k;
+var lengthTest;
+var commonLetterTest;
+
+for (i = 0; i < boxIDs.length; i++) {
+  for (j = i+1; j < boxIDs.length; j++) {
+    commonLetterTest = "";
+    for (k = 0; k < boxIDs[i].length; k++) {
+      if (boxIDs[i][k] == boxIDs[j][k]) {
+        commonLetterTest = commonLetterTest + boxIDs[i][k];
+      }
+    }
+    if (commonLetterTest.length > result.maxLength) {
+      result.maxLength = commonLetterTest.length;
+      result.id1 = boxIDs[i];
+      result.id2 = boxIDs[j];
+      result.commonLetters = commonLetterTest;
+    }
+  }
+}
+
+document.writeln('Result: ' + result.commonLetters.toString());
+document.writeln('IDs: ' + result.id1.toString() + ", "
+                  + result.id2.toString());
+document.writeln('Length: ' + result.maxLength.toString());
